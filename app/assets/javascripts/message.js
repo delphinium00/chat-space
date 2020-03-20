838c04f4958610aaaf41fa2e749c918a479ab490
delphinium00 <ssp.7rush5@gamil.com>
2020-03-20 12:45:58 +0900

    message.js編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,44 @@
 $(function(){
+    function buildHTML(message){
+      if ( message.image ) {
+        var html =
+        `<div class="message">
+            <div class="message__upper-info">
+              <div class="message__upper-info__talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-content">
+              <p class="message-content__text">
+                ${message.content}
+              </p>
+            </div>
+            <img src=${message.image} >
+          </div>`
+        return html;
+      } else {
+        var html =
+        `<div class="message">
+            <div class="message__upper-info">
+              <div class="message__upper-info__talker">
+                ${message.user_name}
+              </div>
+              <div class="message__upper-info__date">
+                ${message.created_at}
+              </div>
+            </div>
+            <div class="message-content">
+              <p class="message-content__text">
+                ${message.content}
+              </p>
+            </div>
+          </div>`
+        return html;
+      };
+    }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +51,8 @@ $(function(){
       processData: false,
       contentType: false
     })
+     .done(function(data){
+       var html = buildHTML(data);
+     })
   });
 });
